642e148295b18eabd7729324f1ce37d8392beabb
m-kro <m.barthauer@t-online.de>
2020-07-22 17:28:08 +0200

    convert svg to pdf

--- Maperitive/callMaperitive.bat
+++ Maperitive/callMaperitive.bat
@@ -1,8 +1,12 @@
 SET mypath=%~dp0
 SET scriptpath=%mypath%render.script 
 
-REM break if Maperitive not in PATH
+REM break if Maperitive or Inkscape are not in PATH
 where /q maperitive || Maperitive not found. && EXIT /B
+where /q inkscape || Inkscape not found. && EXIT /B
 
 REM execute
 maperitive -exitafter %scriptpath%
+
+REM export svg to pdf
+inkscape ../Result/BS_Okerumflut.svg --without-gui --export-area-page --export-pdf-version=1.4 --export-pdf=../Result/BS_okerumflut.pdf
